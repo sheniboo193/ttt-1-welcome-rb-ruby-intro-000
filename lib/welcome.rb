6934756f7de12c3392399babc014780460c366fc
Faizah Tasnim <ft.2129@gmail.com>
2020-03-02 19:02:58 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,2 +1,2 @@
-puts "Wellcome to Tic Tac Toe     "# You can see what this file does by running:
+puts "Welcome to Tic Tac Toe"# You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
